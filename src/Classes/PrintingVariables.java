33084e26011427acc354de020199262f4f928177
ryansousa10 <ryan.sousa4000@gmail.com>
2023-08-28 12:54:52 -0300

    first commit

---
+++ src/Classes/PrintingVariables.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class PrintingVariables {
+    /***
+     *
+     * @param args parametro de entrada do método main
+     */
+    public static void main(String[] args){
+        int age = 22;
+        System.out.println("A idade é: " + age);
+    }
+}
